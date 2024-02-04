df10f78b15883bf5bc54d3c64e3da7e78ce2d4f9
Sajtoschips <benceboglari@gmail.com>
2024-02-04 12:49:46 +0100

    sablon kb kesz

---
+++ src/components/AppNav.vue
@@ -0,0 +1,102 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-success-subtle">
+    <div class="container">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <div v-for="menu in menuItems">
+            <li class="nav-item" v-if="menu.isLoggedIn">
+              <router-link class="nav-link" :to="menu.to">{{
+                menu.title
+              }}</router-link>
+            </li>
+          </div>
+          <li class="nav-item" v-if="!status.loggedIn">
+            <div class="dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i class="fas fa-user"></i>
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <router-link class="dropdown-item" to="/bejelentkezes"
+                    >Bejelentkezés</router-link
+                  >
+                </li>
+                <li>
+                  <router-link class="dropdown-item" to="/regisztracio"
+                    >Regisztráció</router-link
+                  >
+                </li>
+              </ul>
+            </div>
+          </li>
+
+          <li class="nav-item" v-if="status.loggedIn">
+            <a class="nav-link" href="#" @click="onLogout">Kijelentkezés</a>
+          </li>
+        </ul>
+
+        <div v-if="status.loggedIn">
+          {{ user.name }}
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useUserStore } from "../stores/userstore";
+import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
+
+const { status, user } = storeToRefs(useUserStore());
+const { logout } = useUserStore();
+const router = useRouter();
+
+const menuItems = computed(() => {
+  return [
+    {
+      title: "Nyitó oldal",
+      to: "/",
+      isLoggedIn: true,
+    },
+    {
+      title: "Termékek",
+      to: "/blogok",
+      isLoggedIn: true,
+    },
+    {
+      title: "Új blog",
+      to: "/ujblog",
+      isLoggedIn: status.value.loggedIn,
+    },
+  ];
+});
+
+function onLogout() {
+  logout().then(() => {
+    router.push("/");
+  });
+}
+</script>
+
+<style lang="scss" scoped></style>
